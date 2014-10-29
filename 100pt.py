1328e74ea9bed4b68fba1f267e575a93663d77d3
savage-mp6767 <liegrowlmuzak@gmail.com>
2014-10-29 15:10:36 -0500

    one button toggles direction

--- 100pt.py
+++ 100pt.py
@@ -13,7 +13,7 @@ root = Tk()
 
 drawpad = Canvas(root, width=480,height=320, background='white')
 oval = drawpad.create_oval(160,160,320,320, fill="red")
-
+speed = -1
 class MyApp:
 	def __init__(self, parent):
 	        global drawpad
@@ -31,20 +31,33 @@ class MyApp:
 						
 		self.button1.bind("<Button-1>", self.button1Click) 
 		drawpad.pack(side=BOTTOM)
-		
-	def button1Click(self, event):   
-		# Make me move to the left!
-		global oval
-		global drawpad
-		
+		self.animate()
+                
+	def button1Click(self, event):
+	  	# Make me move to the left!
+	  	global speed
 		self.buttonState = not self.buttonState
 		
 		if self.buttonState:
 		    self.button1.configure(text="Left")
+		    speed = -1
 		else:
 		    self.button1.configure(text="Right")
-	
-	# Add the event handler for the second button!
+		    speed = 1
+		    
+	def animate(self):
+            global oval
+            global drawpad
+            global speed
+            
+            x1, y1, x2, y2 = drawpad.coords(oval)
+            if x2 > drawpad.winfo_width() + 160: 
+                drawpad.move(oval,-drawpad.winfo_width() - 160,0)
+            elif x2 < 0:
+                drawpad.move(oval,drawpad.winfo_width() + 160,0)
+	    
+            drawpad.move(oval,speed,0)
+            drawpad.after(1, self.animate)
 	
 		
 myapp = MyApp(root)
